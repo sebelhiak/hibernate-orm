836271b30eb136a598ddd8402c9711cad891e395
sebelhiak <salaheddinebelhiak@gmail.com>
2024-06-28 15:56:33 +0100

    HHH-17846 Handling parametrized types

--- hibernate-core/src/test/java/org/hibernate/orm/test/mapping/basic/OraclePolymorphicJsonTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/mapping/basic/OraclePolymorphicJsonTests.java
@@ -18,7 +18,6 @@ import org.hibernate.testing.orm.junit.ServiceRegistry;
 import org.hibernate.testing.orm.junit.SessionFactory;
 import org.hibernate.testing.orm.junit.SessionFactoryScope;
 import org.hibernate.testing.orm.junit.Setting;
-import org.hibernate.testing.orm.junit.SkipForDialect;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
